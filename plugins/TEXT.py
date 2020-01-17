53df80238fe65ab26018898757707d7b5f0b05af
fartsren123 <30980425+fartsren123@users.noreply.github.com>
2020-01-17 16:47:00 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -1,7 +1,7 @@
 
 
 # Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
-drive_folder_name = "Drive"
+drive_folder_name = "Mitsuha'sDrive"
 
 
 # Enter Your Mega email And Pass (Required)
@@ -11,63 +11,28 @@ drive_folder_name = "Drive"
 
 START = " Hello,<b> {} </b>.\n \nI'm a Drive Uploader Bot. I can upload files to your Google Drive remotely. \n \nTo get started click on /auth and follow the provided instructions. "
 
-HELP = """   <b>AUTHORISE BOT</b> 
-       Use  /auth Command Generate
+HELP = """   <b>Authorise Bot: </b> \n
+       Use <b> /auth </b>Command Generate
        Your Google Drive Token And 
        Send It To Bot  
-<b> You Wanna Change Your Login 
-        Account ?</b> \n
-        You Can Use /revoke 
-        command            
-<b>What I Can Do With This Bot? </b>
-            You Can Upload Any Internet
-            Files On Your google
-            Drive Account.
-<b> Links Supported By Bot</b>
-            * Direct Links 
-            * Openload links [Max Speed 
-              500 KBps :(   ]
-            * Dropbox links 
-            *  Mega links
-            
-            + More On Its way:)
-                
-Bug Report @aryanvikash
+<b> To change Google Drive Account :</b> \n
+        Use /revoke command to get new Token from different Drive Account.          
         """
-DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
-OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow"
-PROCESSING = "Processing Your Request ...!!"
+PROCESSING = "Verifying if Download Link is Valid...!!"
 DOWN_TWO = True
 DOWNLOAD = "Downloading Started ..."
-DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow :("
 DOWN_COMPLETE = "Downloading complete !!"
-NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n @aryanvikash"
-REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @aryanvikash "
+NOT_AUTH = "You Are Not Authorised To Use this Bot \n\n Please Authorise Me Using /auth  \n"
+REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n"
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n report At @aryanvikash "
-AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
-UPLOADING = "Download Complete !! \n Uploading Your file"
-REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
+ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke .\n "
+AUTH_URL = '<a href ="{}">Get Auth Token</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
+UPLOADING = "Download Complete !! \n Uploading to Google Drive"
+REVOKE_TOK = " Your Token was Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Account. "
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At @aryanvikash"
-OPENLOAD = True
-DROPBOX = True
-MEGA = True
-
-
-UPDATE = """ <b> Update  on  27.07.2019</b>
-            * MEGA LINK added
-            * Error Handling Improved
-
-<b> Links Supported By Bot</b>
-            * Direct Links 
-            * Openload links [Max Speed 
-              500 KBps :(   ]
-            * Dropbox links 
-            *  Mega links (only files)
-            
-            + More are in way:) """
-
-ABOUT = " Hello,<b> {} </b>.\n \nI'm a Drive Uploader Bot. I can upload files to your Google Drive remotely. \n \nTo get started click on /auth and follow the provided instructions. "
+AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re-Authorise Me  \n"
+OPENLOAD = False
+DROPBOX = False
+MEGA = Flase
